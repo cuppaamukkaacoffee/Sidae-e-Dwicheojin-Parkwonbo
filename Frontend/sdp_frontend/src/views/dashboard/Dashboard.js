ee58cf6d87e62f3e3012d33de735271c64e52b8d
dury008 <dury008@naver.com>
2021-03-16 14:50:40 +0900

    modify Dashboard

--- Frontend/sdp_frontend/src/views/dashboard/Dashboard.js
+++ Frontend/sdp_frontend/src/views/dashboard/Dashboard.js
@@ -20,17 +20,21 @@ import {
   CNav,
   CNavItem,
   CNavLink,
+  CButton,
+  CBadge
 } from '@coreui/react'
 import {
   CChartBar,
   CChartDoughnut,
 } from '@coreui/react-chartjs'
 import CIcon from '@coreui/icons-react'
+import history from "src/utils/history";
 
 const labels = ['SQL Injection', 'XSS', 'Open Redirect', 'Windows Directory Traversal', 'Linux Directory Traversal', 'LFI Check', 'RFI Check', 'RCE Linux Check', 'SSTI Check']
 const fields = ['target','timestamp']
+const vul_field = ['vul', 'num']
 const days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-
+const backgroundColor = ['#41B883','#E46651','#00D8FF','#DD1B16','#FF4500','#FF8C00','#7CFC00','#4169E1','#4B0082']
 
 const Dashboards = () => {
   const dispatch = useDispatch();
@@ -80,6 +84,14 @@ const Dashboards = () => {
     n = n + '';
     return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
   }
+
+  const num_click = (vul) =>{
+    history.push({
+      pathname: "/vulnerabilities",
+      state: {vul: vul}
+    });
+  }
+
 
   const datasets_scanned = (()=>{
     let data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -137,18 +149,7 @@ const Dashboards = () => {
     return [
       {
         data: data,
-        backgroundColor: [
-          '#41B883',
-          '#E46651',
-          '#00D8FF',
-          '#DD1B16',
-          '#FF4500',
-          '#FF8C00',
-          '#7CFC00',
-          '#4169E1',
-          '#4B0082'
-        ],
-        
+        backgroundColor: backgroundColor,
       }
     ]
   })()
@@ -161,6 +162,13 @@ const Dashboards = () => {
     return String(num)
   })()
 
+  const top_vul = (()=> {
+    let top_vul = []
+    for(let i = 0 ; i < 9 ; i++){
+      top_vul.push({vul : labels[i], num : filterItems_vulnerability(labels[i]).length});
+    }
+    return top_vul
+  })()
 
   return (
     <>
@@ -242,7 +250,7 @@ const Dashboards = () => {
           </CCard>
       </CCol>
       <CCol sm="12" md="6">
-        <CCard style={{height:"400px",overflow: 'auto'}}>
+        <CCard style={{height:"400px",overflow: 'auto'}} accentColor="danger">
           <CTabs> 
             <CNav variant="tabs">
                 <CNavItem>
@@ -288,13 +296,19 @@ const Dashboards = () => {
         </CCard>
         
         
-        <CCard style={{height:"400px",overflow: 'auto'}} accentColor="danger">
+        <CCard style={{height:"500px",overflow: 'auto'}} accentColor="danger">
           <CCardHeader style = {{color: 'black'}}>
             Top Vulnerabilities
           </CCardHeader>
           <CCardBody>
             <CDataTable
               size="sm"
+              items={top_vul}
+              fields={vul_field}
+              header = {false}
+              scopedSlots = {{'num':
+                              (item) => (<td><CButton onClick = {()=>{num_click(item.vul);}} disabled={!item.num}><strong style = {{color: 'red'}}>{item.num}</strong></CButton></td>)
+              }}
             />
           </CCardBody>
         </CCard>
