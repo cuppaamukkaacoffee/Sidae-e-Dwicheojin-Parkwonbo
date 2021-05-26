0bd2d94cd9e8813f1b50f504d09a4f5f4a18b0a7
MORTY <srt3000@hanyang.ac.kr>
2021-05-26 16:05:07 +0900

    Update start.sh

--- Backend/sdp_backend/start.sh
+++ Backend/sdp_backend/start.sh
@@ -4,6 +4,6 @@ dockerize -wait tcp://db:5432 -timeout 20s
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn --preload sdp_backend.asgi:application -k uvicorn.workers.UvicornWorker
+# gunicorn --preload sdp_backend.asgi:application -k uvicorn.workers.UvicornWorker
 
-# python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
